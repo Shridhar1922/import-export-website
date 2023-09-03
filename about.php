5884d7dff40d411bd5d12bcd5658c7ed02dda2d8
supriyamhaishale21 <supriya.mhaishale@corecotechnologies.com>
2023-09-03 13:02:52 +0530

    modfication in about pages

--- about.php
+++ about.php
@@ -16,29 +16,11 @@
     <div class="container">
 
         <div class="row no-margin-l-r">
-            <div class="col-lg-6 col-md-12 col-sm-12 team-box">
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-sm-12 ">
-                        <div class="management-item">
-                            <div class="mngmt-id">
-                                <img src="./assets/img/team/SHASHIKANT_UNAVANE.jpg" alt="md">
-                            </div>
-                            <h3>SHASHIKANT K. UNAVANE </h3>
-                            <p>FOUNDER & M.D.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-sm-12">
-                        <div class="management-item">
-                            <div class="mngmt-id">
-                                <img src="./assets/img/team/RAVIKANT_UNAVANE.jpg" alt="md">
-                            </div>
-                            <h3>RAVIKANT K.UNAVANE </h3>
-                            <p>MARKETING HEAD</p>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-4 col-md-12 col-sm-12 text-center">
+                 <img src="./assets/img/team/tradmark.jpeg" height="100%" class="no-pad">
+                       
             </div>
-            <div class="col-lg-6 col-md-12 info-right founder-content-box">
+            <div class="col-lg-8 col-md-12 info-right founder-content-box">
                 <div class="founder-stmt">
                     <p>An engineer by education, to be precise, Degree holder in Mechanical Engineering.
                         Managing Director is an Active member of Global Import and Export Association.</p>
@@ -54,7 +36,7 @@
         <div class="row align-items-center">
 
             <div class="col-lg-6 col-md-12 info-right" data-aos="fade-right" data-aos-duration="1500">
-                <h1>We Provide Good Quality Products & Services For You</h1>
+                <h1>Supply The Best organic Products Since 2022</h1>
                 <p class="text-justify">
                     Shramuk Overseas is working in the areas of merchant import and export in India. Though there is no
                     limitation for what we trade. Our main aim is to bring all Indian specialities, cultuer, taste,
@@ -93,15 +75,13 @@
             <div class="col-lg-6 col-md-6 col-sm-12" data-aos="zoom-out-up">
                 <div class="vision-mission-box">
                     <h1>Our Vision</h1>
-                    <p>Our vision is to see global economical growth and elevated quality of living through
-                        international business and commerce.</p>
+                    <p>Our vision is to use the right blend of knowledge and expertise to create unbreakable relation with worldeide importers. We are committed to meet customer needs and offer best quality products with competitive prices on prompt delivery and complete satisfaction at all the time.</p>
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12" data-aos="zoom-out-up">
                 <div class="vision-mission-box">
                     <h1>Our Mission</h1>
-                    <p>The mission of “Shramuk Overseas” is connect the world with contributing culture & taste ,&
-                        enhance trust with regards to INDIA in International Trade.</p>
+                    <p>To become the benchmark manufacture export company providing world class products and excellent customer satisfaction through continuous improovement driven by the integrity, teamwork and creativity. Mission of our company is to treat all our clients equally. All our clients are precious to us. To recognize as the India's most famous trading company of peanuts. </p>
                 </div>
             </div>
         </div>
